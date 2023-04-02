ac3fae724212319eb0862c9bc949d49c226c0f83
fartem <jaman.smlnsk@gmail.com>
2023-04-02 13:59:19 +0300

    2023-04-02 v. 1.1.1: added tests

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'minitest', '5.18.0'
 gem 'rake', '12.3.3'
 gem 'rubocop', '1.7'
---
+++ test/test_checker.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/distincter2/checker.rb'
+
+require 'minitest/autorun'
+
+class D2CheckerTest < Minitest::Test
+  def test_valid_file
+    checker = ::Distincter2::D2Checker.new
+    duplicates = checker.analyze_file("#{Dir.pwd}/test_assets/valid_list.md")
+
+    assert duplicates.empty?
+  end
+
+  def test_invalid_file
+    checker = ::Distincter2::D2Checker.new
+    duplicates = checker.analyze_file("#{Dir.pwd}/test_assets/invalid_list.md")
+
+    assert !duplicates.empty?
+  end
+end
